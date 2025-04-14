e4fa4c3b87be437a8ff66743ee65d24232acc241
mishalyn-mei-ilmeng <mishalyn@hawaii.edu>
2025-04-13 18:05:54 -1000

    added icons and allergy names

--- src/app/allergies/page.tsx
+++ src/app/allergies/page.tsx
@@ -1,6 +1,10 @@
 'use client';
 
 import { Col, Container, Row } from 'react-bootstrap';
+import { GiPeanut, GiWheat, GiSesame, GiMilkCarton } from 'react-icons/gi';
+import { FaSeedling, FaEgg, FaFish, FaBreadSlice } from 'react-icons/fa';
+import { TbBottleFilled } from 'react-icons/tb';
+import { FaShrimp } from 'react-icons/fa6';
 
 /** The Allergy page. */
 const Allergy = () => (
@@ -17,6 +21,66 @@ const Allergy = () => (
             we can ensure that you have access to meals that suit your needs and provide a
             worry-free dining experience.
           </p>
+        </Col>
+      </Row>
+      <Row className="align-middle text-center">
+        <Col className="text-center px-5">
+          <p>
+            Please take a moment to review the list of common allergens below.
+            If you have any allergies, please let us know by filling out the form.
+          </p>
+        </Col>
+      </Row>
+      <Row className="justify-content-center text-center">
+        <Col xs={2}>
+          <div>
+            <GiPeanut size={32} />
+            <p>Peanuts</p>
+          </div>
+          <div>
+            <FaSeedling size={32} />
+            <p>Tree Nuts</p>
+          </div>
+        </Col>
+        <Col xs={2}>
+          <div>
+            <GiMilkCarton size={32} />
+            <p>Milk</p>
+          </div>
+          <div>
+            <FaEgg size={32} />
+            <p>Eggs</p>
+          </div>
+        </Col>
+        <Col xs={2}>
+          <div>
+            <FaFish size={32} />
+            <p>Fish</p>
+          </div>
+          <div>
+            <FaShrimp size={32} />
+            <p>Shellfish</p>
+          </div>
+        </Col>
+        <Col xs={2}>
+          <div>
+            <GiWheat size={32} />
+            <p>Wheat</p>
+          </div>
+          <div>
+            <FaBreadSlice size={32} />
+            <p>Gluten</p>
+          </div>
+        </Col>
+        <Col xs={2}>
+          <div>
+            <GiSesame size={32} />
+            <p>Sesame</p>
+          </div>
+          <div>
+            <TbBottleFilled size={32} />
+            <p>Mustard</p>
+          </div>
         </Col>
       </Row>
     </Container>
