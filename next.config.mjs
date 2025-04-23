6820474cf0831d3576db015c63a43181efb45c4a
Shade Matsumoto <shadejsm@gmail.com>
2025-04-23 02:30:25 -1000

    Reverted

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    NEXTAUTH_URL:
-      process.env.VERCEL_ENV === 'production'
-        ? 'https://da-grindz.vercel.app'
-        : process.env.VERCEL_ENV === 'preview'
-        ? `https://${process.env.VERCEL_URL}`
-        : 'http://localhost:3000',
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
