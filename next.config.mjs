8644323bf6b32ec73312e6bf82efd4c5fe480fd8
Shade Matsumoto <shadejsm@gmail.com>
2025-04-23 02:13:27 -1000

    Fix Vercel login error.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXTAUTH_URL:
+      process.env.VERCEL_ENV === 'production'
+        ? 'https://da-grindz.vercel.app'
+        : process.env.VERCEL_ENV === 'preview'
+        ? `https://${process.env.VERCEL_URL}`
+        : 'http://localhost:3000',
+  },
+};
 
 export default nextConfig;
