cf8f16c6d2fdc7d91c5c2244c1e6783a0707a8e2
mishalyn-mei-ilmeng <mishalyn@hawaii.edu>
2025-04-22 19:12:56 -1000

    updated signin with style

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { signIn } from 'next-auth/react';
-import { Button, Card, Col, Container, Form, Row } from 'react-bootstrap';
+import { Button, Card, CardHeader, Col, Container, Form, Row } from 'react-bootstrap';
 
 /** The sign in page. */
 const SignIn = () => {
@@ -29,8 +29,11 @@ const SignIn = () => {
       <Container>
         <Row className="justify-content-center">
           <Col xs={5}>
-            <h1 className="text-center">Sign In</h1>
             <Card>
+              <CardHeader>
+                <h1 className="text-center pt-2">Sign In</h1>
+                <h4 className="text-center">Welcome back!</h4>
+              </CardHeader>
               <Card.Body>
                 <Form method="post" onSubmit={handleSubmit}>
                   <Form.Group controlId="formBasicEmail">
