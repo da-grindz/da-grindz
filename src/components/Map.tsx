e55d2e02ccf13ad517828ca8983ff06ab6c7084e
Shade Matsumoto <shadejsm@gmail.com>
2025-04-24 20:53:52 -1000

    Made markers

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -4,63 +4,54 @@
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { LatLngExpression, LatLngBounds } from 'leaflet';
-import { useEffect, useState } from 'react';
 import { Eatery, VendingMachine } from '@prisma/client';
 
 const center: LatLngExpression = [21.299, -157.816];
 const bounds: LatLngBounds = new LatLngBounds([21.289, -157.824], [21.309, -157.808]);
 
-export default function Map() {
-  const [eateries, setEateries] = useState<Eatery[]>([]);
-  const [vendingMachines, setVendingMachines] = useState<VendingMachine[]>([]);
+type Props = {
+  eateries: Eatery[];
+  vendingMachines: VendingMachine[];
+};
 
-  useEffect(() => {
-    async function fetchData() {
-      const res = await fetch('/api/locations');
-      const data = await res.json();
-      setEateries(data.eateries);
-      setVendingMachines(data.vendingMachines);
-    }
-    fetchData();
-  }, []);
+const Map = ({ eateries, vendingMachines }: Props) => (
+  <MapContainer
+    center={center}
+    zoom={16}
+    scrollWheelZoom
+    style={{ height: '100%' }}
+    maxBounds={bounds}
+  >
+    <TileLayer
+      minZoom={16}
+      attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
 
-  return (
-    <MapContainer
-      center={center}
-      zoom={16}
-      scrollWheelZoom
-      style={{ height: '100%' }}
-      maxBounds={bounds}
-    >
-      <TileLayer
-        minZoom={16}
-        attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+    {eateries.map((eatery) => (
+      <Marker key={`eatery-${eatery.id}`} position={[eatery.y, eatery.x]}>
+        <Popup>
+          <strong>{eatery.name}</strong>
+          <br />
+          {eatery.location}
+          <br />
+          {eatery.hours}
+        </Popup>
+      </Marker>
+    ))}
 
-      {eateries.map((eatery) => (
-        <Marker key={`eatery-${eatery.id}`} position={[eatery.y, eatery.x]}>
-          <Popup>
-            <strong>{eatery.name}</strong>
-            <br />
-            {eatery.location}
-            <br />
-            {eatery.hours}
-          </Popup>
-        </Marker>
-      ))}
+    {vendingMachines.map((vm) => (
+      <Marker key={`vm-${vm.id}`} position={[vm.y, vm.x]}>
+        <Popup>
+          <strong>{`Vending (${vm.type})`}</strong>
+          <br />
+          {vm.location}
+          <br />
+          {vm.hours}
+        </Popup>
+      </Marker>
+    ))}
+  </MapContainer>
+);
 
-      {vendingMachines.map((vm) => (
-        <Marker key={`vm-${vm.id}`} position={[vm.y, vm.x]}>
-          <Popup>
-            <strong>{`Vending (${vm.type})`}</strong>
-            <br />
-            {vm.location}
-            <br />
-            {vm.hours}
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
-  );
-}
+export default Map;
