d38b2bef18de1f8d7a367901aa7f8dee975b2a4d
mishalyn-mei-ilmeng <mishalyn@hawaii.edu>
2025-05-05 01:06:56 -1000

    Added 2 items per mood and visually updated the page.

--- src/components/MiniMoodzGrid.tsx
+++ src/components/MiniMoodzGrid.tsx
@@ -7,11 +7,12 @@ type Item = {
   image: string;
   alt: string;
   title: string;
+  subtitle: string;
 };
 
 const MiniMoodzGrid = ({ items }: { items: Item[] }) => (
   <Container id="mini-moodz" className="justify-content-center py-4">
-    <Row xs={1} sm={2} md={4} className="g-3">
+    <Row xs={1} sm={1} md={2} className="g-2">
       {items.map((item) => (
         <Col key={item.id}>
           <Card id="mini-moodz" className="h-100">
@@ -19,10 +20,12 @@ const MiniMoodzGrid = ({ items }: { items: Item[] }) => (
               variant="top"
               src={item.image}
               alt={item.alt}
-              style={{ height: '150px', objectFit: 'cover' }} // Smaller image size
+              style={{ height: '250px', objectFit: 'cover' }} // Smaller image size
             />
             <Card.Body>
               <Card.Title>{item.title}</Card.Title>
+              <hr id="break" />
+              <Card.Subtitle>{item.subtitle}</Card.Subtitle>
             </Card.Body>
           </Card>
         </Col>
