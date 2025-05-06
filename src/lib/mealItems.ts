1b5b5360e39057e7e6c9faf12b00504d381dd82d
Sydney Hashiro <sydhashiro@gmail.com>
2025-05-06 11:52:38 -1000

    Add macros goal input
    - added a box to put your weekly macro goals
    - added menu items to the grindz vault
    - added total calories progress bar

---
+++ src/lib/mealItems.ts
@@ -0,0 +1,196 @@
+export const mealItems = [
+  {
+    name: 'Egg, Pesto, and Mozzarella Sandwich',
+    vendor: 'Starbucks',
+    macros: { protein: 21, carbs: 36, fats: 16 },
+  },
+  {
+    name: 'Greens n Ginger Smoothie',
+    vendor: 'Jamba Juice',
+    macros: { protein: 4, carbs: 70, fats: 1 },
+  },
+  {
+    name: 'Veggie Delight',
+    vendor: 'Subway',
+    macros: { protein: 10, carbs: 39, fats: 6 },
+  },
+  {
+    name: 'Veggie Spring Rolls',
+    vendor: 'Panda Express',
+    macros: { protein: 7, carbs: 38, fats: 12 },
+  },
+  {
+    name: 'Kunia Bowl',
+    vendor: 'Bowlful',
+    macros: { protein: 35, carbs: 95, fats: 40 },
+  },
+  {
+    name: 'Bean & Rice Burrito',
+    vendor: "B'rito Bowl",
+    macros: { protein: 23, carbs: 70, fats: 14 },
+  },
+  {
+    name: 'Tofu Salad',
+    vendor: 'Bale',
+    macros: { protein: 18, carbs: 4.7, fats: 26 },
+  },
+  {
+    name: 'Tofu Pho',
+    vendor: 'Bale',
+    macros: { protein: 20, carbs: 43, fats: 12 },
+  },
+  {
+    name: 'Furikake Fish Plate',
+    vendor: 'Campus Center Food Court',
+    macros: { protein: 30, carbs: 95, fats: 26 },
+  },
+  {
+    name: 'Musubis',
+    vendor: 'Holo Holo Bistro',
+    macros: { protein: 7, carbs: 58, fats: 0.5 },
+  },
+  {
+    name: 'Mini Chicken Katsu Plate',
+    vendor: 'L&L',
+    macros: { protein: 37, carbs: 97, fats: 50 },
+  },
+  {
+    name: 'Garlic Chicken Bento',
+    vendor: 'Holo Holo Bistro',
+    macros: { protein: 36, carbs: 95, fats: 35 },
+  },
+  {
+    name: 'BBQ Mix Plate',
+    vendor: 'L&L',
+    macros: { protein: 70, carbs: 106, fats: 61 },
+  },
+  {
+    name: 'Loco Moco Plate',
+    vendor: 'L&L',
+    macros: { protein: 55, carbs: 102, fats: 80 },
+  },
+  {
+    name: 'Kimchi Fried Rice',
+    vendor: 'Campus Center Food Court',
+    macros: { protein: 13, carbs: 40, fats: 12 },
+  },
+  {
+    name: 'Egg Fried Rice',
+    vendor: 'Campus Center Food Court',
+    macros: { protein: 11, carbs: 45, fats: 12 },
+  },
+  {
+    name: 'Grilled Teriyaki Chicken',
+    vendor: 'Panda Express',
+    macros: { protein: 33, carbs: 14, fats: 10 },
+  },
+  {
+    name: 'Grilled Chicken Wrap',
+    vendor: 'Subway',
+    macros: { protein: 42, carbs: 54, fats: 11 },
+  },
+  {
+    name: 'BBQ Chicken Plate',
+    vendor: 'L&L',
+    macros: { protein: 66, carbs: 100, fats: 55 },
+  },
+  {
+    name: "B'rito Bowl",
+    vendor: "B'rito Bowl",
+    macros: { protein: 35, carbs: 62, fats: 28 },
+  },
+  {
+    name: 'Warrior Cheeseburger',
+    vendor: 'Campus Center Food Court',
+    macros: { protein: 45, carbs: 60, fats: 42 },
+  },
+  {
+    name: 'Beef Pho',
+    vendor: 'Bale',
+    macros: { protein: 47, carbs: 80, fats: 15 },
+  },
+  {
+    name: 'Yakitori Chicken Skewers',
+    vendor: 'Bowlful',
+    macros: { protein: 30, carbs: 5, fats: 4 },
+  },
+  {
+    name: 'Orange Dream Machine',
+    vendor: 'Jamba Juice',
+    macros: { protein: 6, carbs: 93, fats: 0 },
+  },
+  {
+    name: 'Aloha Pineapple',
+    vendor: 'Jamba Juice',
+    macros: { protein: 7, carbs: 92, fats: 0 },
+  },
+  {
+    name: 'Caramel Frappuccino',
+    vendor: 'Starbucks',
+    macros: { protein: 4, carbs: 67, fats: 15 },
+  },
+  {
+    name: 'Hot Chai Latte',
+    vendor: 'Starbucks',
+    macros: { protein: 8, carbs: 45, fats: 4.5 },
+  },
+  {
+    name: 'Iced Matcha Tea Latte',
+    vendor: 'Starbucks',
+    macros: { protein: 9, carbs: 27, fats: 5 },
+  },
+  {
+    name: 'Taro Milk Tea',
+    vendor: 'Ding Tea',
+    macros: { protein: 2, carbs: 94, fats: 11 },
+  },
+  {
+    name: 'Black Tea Latte',
+    vendor: 'Ding Tea',
+    macros: { protein: 6, carbs: 48, fats: 4 },
+  },
+  {
+    name: 'Lavender Lemonade',
+    vendor: 'Holo Holo Bistro',
+    macros: { protein: 0, carbs: 60, fats: 0 },
+  },
+  {
+    name: 'Chocolate Croissant',
+    vendor: 'Starbucks',
+    macros: { protein: 5, carbs: 34, fats: 18 },
+  },
+  {
+    name: 'Double Chocolate Brownie',
+    vendor: 'Starbucks',
+    macros: { protein: 6, carbs: 55, fats: 28 },
+  },
+  {
+    name: '(1x) Mochi-sadas',
+    vendor: 'Quick Bites',
+    macros: { protein: 1, carbs: 18, fats: 1 },
+  },
+  {
+    name: 'Vanilla Milk Chocolate Almond Ice Cream Bar',
+    vendor: 'Quick Bites',
+    macros: { protein: 4, carbs: 20, fats: 19 },
+  },
+  {
+    name: 'Acai Bowl',
+    vendor: 'Jamba Juice',
+    macros: { protein: 7, carbs: 102, fats: 9 },
+  },
+  {
+    name: 'Belgian Waffle',
+    vendor: 'Jamba Juice',
+    macros: { protein: 4, carbs: 35, fats: 14 },
+  },
+  {
+    name: 'Footlong Cookie',
+    vendor: 'Subway',
+    macros: { protein: 14, carbs: 181, fats: 61 },
+  },
+];
+
+export const mealMacros = Object.fromEntries(
+  mealItems.map((item) => [item.name, item.macros]),
+);
