e7632de082cece371ff8fd67e0794898cd1c6c42
Shade Matsumoto <shadejsm@gmail.com>
2025-05-08 22:07:17 -1000

    cleaned up unused components

--- src/lib/validationSchemas.ts
+++ src/lib/validationSchemas.ts
@@ -1,20 +1,5 @@
 import * as Yup from 'yup';
 
-export const AddStuffSchema = Yup.object({
-  name: Yup.string().required(),
-  quantity: Yup.number().positive().required(),
-  condition: Yup.string().oneOf(['excellent', 'good', 'fair', 'poor']).required(),
-  owner: Yup.string().required(),
-});
-
-export const EditStuffSchema = Yup.object({
-  id: Yup.number().required(),
-  name: Yup.string().required(),
-  quantity: Yup.number().positive().required(),
-  condition: Yup.string().oneOf(['excellent', 'good', 'fair', 'poor']).required(),
-  owner: Yup.string().required(),
-});
-
 export const PreferencesSchema = Yup.object({
   allergies: Yup.array().of(Yup.string().required()).required().default([]),
   mood: Yup.string().required('Choose your mood'),
